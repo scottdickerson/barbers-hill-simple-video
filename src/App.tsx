8763c6309a221b65d6b2ceb29133a6016e04f73d
Scott Dickerson <swiftfoot@hotmail.com>
2022-09-04 09:01:44 -0500

    preload image

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import { PullScreen } from "./components/PullScreen/PullScreen";
 import { VideoPlayer } from "./components/VideoPlayer/VideoPlayer";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { ROUTES } from "./constants";
+import { Helmet } from "react-helmet";
 
 function App() {
   const navigate = useNavigate();
 
   return (
     <div className="App">
+      <Helmet>
+        <link
+          rel="preload"
+          href={`/images/${process.env.REACT_APP_VIDEO_STILL_IMAGE_NAME}`}
+          as="image"
+        ></link>
+        <link
+          rel="preload"
+          href={`/videos/${process.env.REACT_APP_VIDEO_NAME}`}
+          as="video"
+          type="video/mp4"
+        ></link>
+      </Helmet>
       <Routes>
         <Route
           path={ROUTES.PULLSCREEN_URL}
